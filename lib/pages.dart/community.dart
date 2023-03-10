68fe5f6f4d3072f74471cab1fc85b403b6747eb3
Jaya Shrestha <jayastha243@gmail.com>
2023-03-10 11:53:34 +0545

    Drawer Added

---
+++ lib/pages.dart/community.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CommunityScreen extends StatelessWidget {
+  const CommunityScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
